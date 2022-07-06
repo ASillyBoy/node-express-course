33172cbe0f6896305275cd6fe7e5e1857dbdf928
Jiayan Xu <rayjiayanxu@hotmail.com>
2022-07-06 19:11:36 -0400

    Lecture 184

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
-require('express-async-errors');
+require('express-async-errors');      //Default express error handler
 
-const express = require('express');
+const express = require('express');   //import express
 const app = express();
-
+//Router
+const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
-app.use(express.json());
+app.use(express.json());              //this is for using the post method (req.body) !!must have otherwise throw error
 
+app.use('/api/v1', mainRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+//Below server code
 const port = process.env.PORT || 3000;
 
 const start = async () => {
